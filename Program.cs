e2ff20946948412b54f5fe680cbf1e6c04d42260
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-29 17:03:54 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace petlje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite niz znakova:");
+            string nizZnakova = Console.ReadLine();
+
+            char najcesciZnak = PronadjiNajcesciZnak(nizZnakova, out int brojPojavljivanja);
+
+            Console.WriteLine($"Najcesci znak je: {najcesciZnak}");
+            Console.WriteLine($"Broj pojavljivanja: {brojPojavljivanja}");
+        }
+
+        static char PronadjiNajcesciZnak(string nizZnakova, out int brojPojavljivanja)
+        {
+            int[] brojac = new int[256]; // Pretpostavljamo ASCII znakove
+
+            foreach (char znak in nizZnakova)
+            {
+                brojac[znak]++;
+            }
+
+            char najcesciZnak = ' ';
+            brojPojavljivanja = 0;
+
+            for (int i = 0; i < 256; i++)
+            {
+                if (brojac[i] > brojPojavljivanja)
+                {
+                    najcesciZnak = (char)i;
+                    brojPojavljivanja = brojac[i];
+                }
+            }
+            Console.ReadKey();
+
+            return najcesciZnak;
+        }
+    }
+}
